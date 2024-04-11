78bfda1f45859570d8c3b4001a5dabb3cedf1176
sinhngocdo <sinhngocdo@gmail.com>
2024-04-11 23:03:04 +0700

    fix code

--- Assets/Scripts/ShootHandle/Bow.cs
+++ Assets/Scripts/ShootHandle/Bow.cs
@@ -2,27 +2,42 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bow : MonoBehaviour
 {
     Vector2 direction;
+    
+    [SerializeField] private InputAction screenPos;
 
-    [SerializeField] float force;
+    private Vector2 curMousePos;
+    private Vector2 bowPos;
+    
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
+        screenPos.Enable();
 
+        screenPos.performed += OnMousePos;
     }
+    private void OnDisable()
+    {
+        screenPos.Disable();
+    }
+
+    private void OnMousePos(InputAction.CallbackContext context)
+    {
+        curMousePos = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>()) ;
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 bowPos = transform.position;
+        bowPos = transform.position;
 
-        direction = mousePos - bowPos;
+        direction = curMousePos - bowPos;
 
         faceMouse();
     }
