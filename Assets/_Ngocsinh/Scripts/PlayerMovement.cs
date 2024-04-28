ece3f1bfc06bf27ac30120270c333cb35a7c99ec
sinhngocdo <sinhngocdo@gmail.com>
2024-04-28 18:13:30 +0700

    update player move

--- Assets/_Ngocsinh/Scripts/PlayerMovement.cs
+++ Assets/_Ngocsinh/Scripts/PlayerMovement.cs
@@ -11,16 +11,24 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movement;
 
     [SerializeField] private float moveSpeed = 0f;
-
+    [SerializeField] private float limitFlyTop = 0f;
+    [SerializeField] private float limitFlyBot = 0f;
+ 
     private void Awake()
     {
         _input = new CustomInput();
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        
+    }
+
     private void OnEnable()
     {
         _input.Enable();
+        
         _input.Player.Move.performed += OnPlayerMovePerformed;
         _input.Player.Move.canceled += OnPlayerMoveCancelled;
     }
@@ -35,7 +43,28 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rb.velocity = _movement * moveSpeed  * Time.deltaTime ;
+        Move();
+    }
+
+    private void Move()
+    {
+        
+        if (transform.position.y >= limitFlyTop)
+        {
+            transform.position = new Vector3(transform.position.x, limitFlyTop - 0.000001f, transform.position.z);
+        }
+
+        else if (transform.position.y <= limitFlyBot)
+        {
+            transform.position = new Vector3(transform.position.x, limitFlyBot + 0.000001f, transform.position.z);
+        }
+        else
+        {
+            Vector2 movement = _movement * moveSpeed  * Time.deltaTime ;
+            _rb.MovePosition(_rb.position + movement);
+        }
+        
+        
     }
 
     private void OnPlayerMovePerformed(InputAction.CallbackContext value)
